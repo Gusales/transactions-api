1eaedd74f9f9025f245c814081abbd70582dd9d6
= <=>
2023-10-20 16:20:45 -0300

    Refact: update InMemoryUserRepository
    Change Function for Class

--- src/repositories/in-memory-repository/in-memory-user-repository.ts
+++ src/repositories/in-memory-repository/in-memory-user-repository.ts
@@ -2,42 +2,35 @@ import { UserType } from '@/@types/UserType'
 import { UserRepository } from '../user-repository'
 import { randomUUID } from 'node:crypto'
 
-export function InMemoryUserRepository(): UserRepository {
-  const users: UserType[] = []
-
-  async function create(data: UserType) {
+export class InMemoryUserRepository implements UserRepository {
+  public Users: UserType[] = []
+  async create(data: UserType) {
     const user: UserType = {
       id: randomUUID(),
-      name: data.name,
       email: data.email,
+      name: data.name,
       password: data.password,
     }
 
-    users.push(user)
+    this.Users.push(user)
     return user
   }
 
-  async function findById(id: string) {
-    const user = users.find((item) => item.id === id)
-
-    if (!user) {
-      return null
-    }
-
-    return user
-  }
-
-  async function findByEmail(email: string) {
-    const user = users.find((item) => item.email === email)
+  async findById(id: string) {
+    const user = this.Users.find((item) => item.id === id)
     if (!user) {
       return null
     }
     return user
   }
 
-  return {
-    create,
-    findById,
-    findByEmail,
+  async findByEmail(email: string) {
+    const user = this.Users.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
   }
 }
