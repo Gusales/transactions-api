5bbea2a8f7f721ad4db225d1f91b475c974e2304
= <=>
2023-10-20 22:05:54 -0300

    Test: Authenticate tests
    Created tests for authenticate use case

---
+++ src/test/authenticate.spec.ts
@@ -0,0 +1,60 @@
+import { InMemoryUserRepository } from '@/repositories/in-memory-repository/in-memory-user-repository'
+import { AuthenticateUseCase } from '@/use-cases/authenticate'
+import { CredentialsError } from '@/use-cases/errors/credentials-error'
+import { hash } from 'bcryptjs'
+import { describe, it, expect, beforeEach } from 'vitest'
+
+describe('Authenticate use case', () => {
+  let userRepository: InMemoryUserRepository
+  let sut: AuthenticateUseCase
+
+  beforeEach(() => {
+    userRepository = new InMemoryUserRepository()
+    sut = new AuthenticateUseCase(userRepository)
+  })
+
+  it('should be able to authenticate', async () => {
+    await userRepository.create({
+      email: 'johndoe@gmail.com',
+      name: 'John Doe',
+      password: await hash('123456', 8),
+    })
+
+    const { user } = await sut.execute({
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate using wrong email', async () => {
+    await userRepository.create({
+      email: 'johndoe@gmail.com',
+      name: 'John Doe',
+      password: await hash('123456', 8),
+    })
+
+    expect(async () => {
+      await sut.execute({
+        email: 'johndoe@outlook.com',
+        password: '123456',
+      })
+    }).rejects.toBeInstanceOf(CredentialsError)
+  })
+
+  it('should not be able to authenticate using wrong password', async () => {
+    await userRepository.create({
+      email: 'johndoe@gmail.com',
+      name: 'John Doe',
+      password: await hash('123456', 8),
+    })
+
+    expect(async () => {
+      await sut.execute({
+        email: 'johndoe@gmail.com',
+        password: '1234567',
+      })
+    }).rejects.toBeInstanceOf(CredentialsError)
+  })
+})
