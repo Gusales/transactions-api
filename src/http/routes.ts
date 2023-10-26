a941cebb61ba3eddddc24425b043acd64b8a22f4
= <=>
2023-10-26 16:57:37 -0300

    refactor: create folder to store application routes

--- src/http/routes.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from 'fastify'
-import { registerController } from './controllers/register-controller'
-
-export async function appRoutes(app: FastifyInstance) {
-  app.post('/sessions', registerController)
-}
