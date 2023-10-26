153f25754a58052a3630195a37fd18299516c30a
= <=>
2023-10-26 05:52:30 -0300

    Fix: add variable page in transaction respository

--- src/repositories/transactions-repository.ts
+++ src/repositories/transactions-repository.ts
@@ -2,5 +2,5 @@ import { TransactionsType } from '@/@types/TransactionsTypes'
 
 export interface TransactionsRepository {
   create: (data: TransactionsType) => Promise<TransactionsType>
-  findByUserId: (id: string) => Promise<TransactionsType[]>
+  findByUserId: (id: string, page: number) => Promise<TransactionsType[]>
 }
